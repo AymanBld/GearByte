32bfd5efcf1949075eb8337e489be658d74412e1
Nassim-BENDAOUD <bassimbassim06@gmail.com>
2025-03-24 16:23:35 +0100

    Add files via upload

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
@@ -7,40 +8,58 @@ const Nav = () => {
   return (
     <header className="header">
       <nav className="navbar">
-        <a href="#Hero" className="logo"><span>Gear</span>Byte</a>
+        <a href="#Hero" className="logo">
+          <span>Gear</span>Byte
+        </a>
 
-       
-        <nav className={`navx ${menuOpen ? "show" : ""}`}>
-         <ul className={`nav-links ${menuOpen ? "show" : ""}`}>
-          <li><a href="#Hero">Home</a></li>
-          <li><a href="#Rent">RentPcs</a></li>
-          <li className="dropdown">
-            <a href="#buy">OurProducts<i className="bx bx-chevron-down"></i></a>
-            <ul className="dropdown-menu">
-              <li><a href="#cpu">Monitors</a></li>
-              <li><a href="#Accessoires">Accessories</a></li>
-              <li><a href="#monitors">Desktop</a></li>
-            </ul>
-          </li>
-          <li><a href="#Reviews">Reviews</a></li>
-          <li><a href="#Contactus">Contactus</a></li>
-          
-        </ul>
-      </nav>
+        <div className={`navx ${menuOpen ? "show" : ""}`}>
+          <ul className={`nav-links ${menuOpen ? "show" : ""}`}>
+            <li>
+              <a href="#Hero">Home</a>
+            </li>
+            <li>
+              <a href="#Rent">RentPcs</a>
+            </li>
+            <li className="dropdown">
+              <a href="#buy">
+                OurProducts<i className="bx bx-chevron-down"></i>
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a href="#cpu">Monitors</a>
+                </li>
+                <li>
+                  <a href="#Accessoires">Accessories</a>
+                </li>
+                <li>
+                  <a href="#monitors">Desktop</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#Reviews">Reviews</a>
+            </li>
+            <li>
+              <a href="#Contactus">Contactus</a>
+            </li>
+          </ul>
+        </div>
 
-      <i className="bx bx-cart"><a href=""></a></i>
-        <i 
-          className={`bx bx-menu ${menuOpen ? "active" : ""}`} 
-          id="menu-icon" 
+        <i className="bx bx-cart"></i>
+        <i
+          className={`bx bx-menu ${menuOpen ? "active" : ""}`}
+          id="menu-icon"
           onClick={() => setMenuOpen(!menuOpen)}
         ></i>
-
-        <button className="btn" id='login'><a href="">Login</a></button>
-        <i className='bx bx-user'><a href=""></a></i>
+        <Link to="/login">
+          <button className="btn" id="login">
+            Login
+          </button>
+        </Link>
+        <i className="bx bx-user"></i>
       </nav>
 
-      <input type="text" placeholder="Recherche" className="input"/>
-     
+      <input type="text" placeholder="Search" className="input" />
     </header>
   );
 };
