0c1967aba6a111cabbeeee5534590c18ee054fdc
Rayan <162971209+Rayan-Taibi@users.noreply.github.com>
2025-03-24 12:49:38 +0100

    added home page files
    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/GearByte/',
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+  port : {
+    server : 3000
+  }
 })
