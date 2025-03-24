253670c0c1767bedc543ae60ad40c3c9537307ff
Nassim-BENDAOUD <bassimbassim06@gmail.com>
2025-03-24 16:16:21 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
-import Nav from './assets/components/Nav'
-import Hero from './assets/components/Hero'
-import OurService from './assets/components/OurService'
-import Reviews from './assets/components/Reviews'
-import Footer from './assets/components/Footer'
-import Copyright from './assets/components/copyright'
-import Contactus from './assets/components/Contactus'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Nav from "./assets/components/Nav";
+import Hero from "./assets/components/Hero";
+import OurService from "./assets/components/OurService";
+import Reviews from "./assets/components/Reviews";
+import Footer from "./assets/components/Footer";
+import Copyright from "./assets/components/copyright";
+import Contactus from "./assets/components/Contactus";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+
+const Layout = () => {
+  const location = useLocation();
+  const hideNavOnRoutes = ["/login", "/signup"];
+
+  return (
+    <>
+      {!hideNavOnRoutes.includes(location.pathname) && <Nav />}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <OurService />
+              <Reviews />
+              <Contactus />
+              <Footer />
+              <Copyright />
+            </>
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </>
+  );
+};
 
 const App = () => {
   return (
-   <>
-    <Nav/>
-    <Hero/>
-    <OurService/>
-    <Reviews/>
-    <Contactus/>
-    <Footer/>
-    <Copyright/>
-   </>
-  )
-}
+    <Router>
+      <Layout />
+    </Router>
+  );
+};
 
-export default App
+export default App;
