08bd19ddc52faddc340d7f4b9b9ef667800aac29
Nassim-BENDAOUD <bassimbassim06@gmail.com>
2025-04-30 21:34:00 +0100

    Update AddPC.jsx

--- src/Pages/dashboard/AddPC.jsx
+++ src/Pages/dashboard/AddPC.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchWithAuth } from '../../utils/fetchWithAuth';
-import Toast from '../../components/Toast';
+import Toast from '../../Components/Toast';
 
 const AddPC = () => {
   const navigate = useNavigate();
